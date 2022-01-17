3c9dcf9aecb939d8ba79d18989af8d2ddefa2285
eghbal <81761081+eghbalm@users.noreply.github.com>
2022-01-17 12:15:53 +0330

    Add files via upload

---
+++ ATA_Tools/IOPortAccess.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ATA_Tools
+{
+    public unsafe class IOPortAccess
+    {
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String DllName);
+
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr hModule, String ProcName);
+
+        [DllImport("kernel32")]
+        private extern static bool FreeLibrary(IntPtr hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool InitializeWinIo();
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool GetPhysLong(IntPtr PhysAddr, UInt32* pPhysVal);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool SetPhysLong(uint PhysAddr, UInt32 PhysVal);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool GetPortVal(uint wPortAddr, UInt32* pdwPortVal, byte bSize);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool SetPortVal(uint wPortAddr, UInt32 pdwPortVal, byte bSize);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool ShutdownWinIo();
+
+        InitializeWinIo InitWinIo = null;
+        GetPortVal GetPVal = null;
+        SetPortVal SetPVal = null;
+        ShutdownWinIo ShtWinIo = null;
+        IntPtr hMod;
+
+        public IOPortAccess()
+        {
+            hMod = LoadLibrary("WinIo.dll");
+
+            if (hMod == IntPtr.Zero)
+            {
+                string msg = "Can't find WinIo.dll. Make sure the WinIo.dll library files are located in the same directory as your executable file.";
+                Console.WriteLine(msg);
+                // throw new Exception(msg);
+            }
+
+            IntPtr pFunc = GetProcAddress(hMod, "InitializeWinIo");
+            if (pFunc != IntPtr.Zero)
+            {
+                InitWinIo = (InitializeWinIo)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(InitializeWinIo));
+                bool result = InitWinIo();
+
+                if (!result)
+                {
+                    FreeLibrary(hMod);
+                    hMod = IntPtr.Zero;
+
+                    string msg = "Error returned from InitializeWinIo. Make sure you are running with administrative privilages are that WinIo library files are located in the same directory as your executable file.";
+                    Console.WriteLine(msg);
+                    // throw new Exception(msg);
+                }
+            }
+        }
+
+        ~IOPortAccess()
+        {
+            if (hMod != IntPtr.Zero)
+            {
+                IntPtr pFunc = GetProcAddress(hMod, "ShutdownWinIo");
+                if (pFunc != IntPtr.Zero)
+                {
+                    this.GetPVal = null;
+                    this.SetPVal = null;
+
+                    ShtWinIo = (ShutdownWinIo)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(ShutdownWinIo));
+                    ShtWinIo();
+
+                    FreeLibrary(hMod);
+                    hMod = IntPtr.Zero;
+                }
+            }
+        }
+
+        public void Close()
+        {
+            if (hMod != IntPtr.Zero)
+            {
+                IntPtr pFunc = GetProcAddress(hMod, "ShutdownWinIo");
+                if (pFunc != IntPtr.Zero)
+                {
+                    this.GetPVal = null;
+                    this.SetPVal = null;
+
+                    ShtWinIo = (ShutdownWinIo)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(ShutdownWinIo));
+                    ShtWinIo();
+
+                    FreeLibrary(hMod);
+                    hMod = IntPtr.Zero;
+                }
+            }
+        }
+
+        public bool GetPortValue(UInt16 portAddress, UInt32* dddd, byte bt)
+        {
+            //UInt32 portValue = 0;
+
+            if (hMod != IntPtr.Zero)
+            {
+                if (this.GetPVal == null)
+                {
+                    IntPtr pFunc = GetProcAddress(hMod, "GetPortVal");
+                    if (pFunc != IntPtr.Zero)
+                    {
+                        this.GetPVal = (GetPortVal)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(GetPortVal));
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (!this.GetPVal(portAddress, dddd, bt))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool SetPortValue(UInt16 portAddress, UInt32 portValue, byte bt)
+        {
+            if (hMod != IntPtr.Zero)
+            {
+                if (this.SetPVal == null)
+                {
+                    IntPtr pFunc = GetProcAddress(this.hMod, "SetPortVal");
+                    if (pFunc != IntPtr.Zero)
+                    {
+                        this.SetPVal = (SetPortVal)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(SetPortVal));
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                bool result = this.SetPVal(portAddress, portValue, bt);
+                if (!result)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool GetBit(UInt32 data, int index)
+        {
+            if ((index < 0) || (index > 31))
+            {
+                string msg = "Bit index must be between 0 and 31.";
+                Console.WriteLine(msg);
+                // throw new ArgumentOutOfRangeException("index", index, msg);
+            }
+            UInt32 mask = (UInt32)1 << index;
+            UInt32 result = data & mask;
+            return (result > 0);
+        }
+
+        public UInt32 SetBit(UInt32 data, int index, bool value)
+        {
+            if ((index < 0) || (index > 31))
+            {
+                string msg = "Bit index must be between 0 and 31.";
+                Console.WriteLine(msg);
+                // throw new ArgumentOutOfRangeException("index", index, msg);
+            }
+            UInt32 result;
+            UInt32 mask = (UInt32)1 << index;
+
+            if (value)
+            {
+                result = data | mask;
+            }
+            else
+            {
+                result = data & ~mask;
+            }
+            return result;
+        }
+    }
+}
---
+++ ATA_Tools/frmPortIoNumber.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace ATA_Tools
+{
+    public partial class frmPortIoNumber : Form
+    {
+        frmMain pn;
+        public frmPortIoNumber(frmMain portnum)
+        {
+            InitializeComponent();
+            pn = portnum;
+            List<PortIO_Info> stt = GetIoPorts();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("BaseAddr");
+            dt.Columns.Add("DeviceID");
+            dt.Columns.Add("Description");
+            for (int i = 0; i < stt.Count; i++)
+            {
+                dt.Rows.Add();
+                dt.Rows[i]["Description"] = stt[i].Caption;
+                dt.Rows[i]["DeviceID"] = stt[i].DeviceID;
+                dt.Rows[i]["BaseAddr"] = stt[i].StartingAddress.ToString("x").ToUpper();
+            }
+            gvPorts.DataSource = dt;
+            gvPorts.Columns[0].Width = 100;
+            gvPorts.Columns[1].Width = 450;
+            gvPorts.Columns[2].Width = 250;
+
+        }
+
+        public const string ATA_ATAPI_GUID = "{4d36e96a-e325-11ce-bfc1-08002be10318}";
+        ManagementScope scope = new ManagementScope(@"\\" + Environment.MachineName + @"\root\CIMV2");
+
+        public struct PortIO_Info
+        {
+            public string DeviceID;
+            public int StartingAddress;
+            public int EndingAddress;
+            public string Caption;
+            public string Service;
+            public string Name;
+        }
+
+        public struct IO_Port
+        {
+            public string DevID;
+            public string Caption;
+            public string Name;
+            public string Antecedent;
+            public string Dependent;
+            public int StartingAddress;
+            public int EndingAddress;
+            public string Service;
+        }
+
+        struct PnPentity
+        {
+            public string Caption;
+            public string ClassGuid;
+            public string Description;
+            public string DeviceID;
+            public string Name;
+            public string Service;
+        }
+
+        public List<PortIO_Info> GetIoPorts()
+        {
+            List<IO_Port> ioPort = new List<IO_Port>();
+            List<PortIO_Info> PIInfo = new List<PortIO_Info>();
+            List<PnPentity> pnPentity = new List<PnPentity>();
+
+            ManagementScope scope = new ManagementScope(@"\\" + Environment.MachineName + @"\root\CIMV2");
+            SelectQuery sqpp = new SelectQuery("Select * From Win32_PnPentity where ClassGuid='" + ATA_ATAPI_GUID + "'");
+            ManagementObjectSearcher searcher1 = new ManagementObjectSearcher(scope, sqpp);
+
+            ManagementObjectCollection moc = searcher1.Get();
+
+            foreach (ManagementObject mo in moc)
+            {
+                if (mo["DeviceID"] != null)
+                {
+                    PnPentity pp = new PnPentity();
+                    pp.DeviceID = mo["DeviceID"].ToString();
+                    pp.Caption = mo["Caption"].ToString();
+                    pp.Description = mo["Description"].ToString();
+                    pp.ClassGuid = mo["ClassGuid"].ToString();
+                    pp.Name = mo["Name"].ToString();
+                    pp.Service = mo["Service"].ToString();
+                    pnPentity.Add(pp);
+                }
+            }
+
+
+            foreach (var item in pnPentity)
+            {
+                SelectQuery sqpr = new SelectQuery("Select * From Win32_PnPAllocatedResource");
+                ManagementObjectSearcher searcher2 = new ManagementObjectSearcher(scope, sqpr);
+
+                ManagementObjectCollection moc1 = searcher2.Get();
+
+                foreach (ManagementObject mo in moc1)
+                {
+                    if (mo["Dependent"] != null)
+                    {
+                        if (mo["Dependent"].ToString().Contains(item.DeviceID.Replace(@"\", @"\\")))
+                        {
+
+                            if (mo["Antecedent"] != null)
+                            {
+                                if (mo["Antecedent"].ToString().Contains("StartingAddress"))
+                                {
+                                    //Antecedent.Add(mo["Antecedent"].ToString());
+
+                                    IO_Port i = new IO_Port();
+                                    i.Antecedent = mo["Antecedent"].ToString();
+                                    i.Dependent = mo["Dependent"].ToString();
+                                    i.DevID = item.DeviceID;
+                                    i.Caption = item.Caption;
+                                    i.Name = item.Name;
+                                    i.Service = item.Service;
+                                    ioPort.Add(i);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            List<String> StartingAddress = new List<String>();
+
+            for (int i = 0; i < ioPort.Count; i++)
+            {
+                SelectQuery sqpr = new SelectQuery("Select * From Win32_PortResource where StartingAddress>1000");
+                ManagementObjectSearcher searcher2 = new ManagementObjectSearcher(scope, sqpr);
+                ManagementObjectCollection moc1 = searcher2.Get();
+
+                foreach (ManagementObject mo in moc1)
+                {
+                    if (mo["StartingAddress"] != null)
+                    {
+                        if (ioPort[i].Antecedent.Contains("\"" + mo["StartingAddress"].ToString() + "\""))
+                        {
+                            int sa = int.Parse(mo["StartingAddress"].ToString());
+                            int ea = int.Parse(mo["EndingAddress"].ToString());
+                            if (ea - sa == 7)
+                            {
+                                PortIO_Info pi = new PortIO_Info();
+                                pi.Caption = ioPort[i].Caption;
+                                pi.DeviceID = ioPort[i].DevID;
+                                pi.EndingAddress = ea;
+                                pi.StartingAddress = sa;
+                                pi.Name = ioPort[i].Name;
+                                pi.Service = ioPort[i].Service;
+                                PIInfo.Add(pi);
+                            }
+                        }
+
+                    }
+                }
+            }
+
+
+            return PIInfo;
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            if (gvPorts.SelectedRows.Count > 0)
+            {
+                pn.txtPort.Text = gvPorts.SelectedRows[0].Cells[0].Value.ToString();                
+            }
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void gvPorts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (gvPorts.SelectedRows.Count > 0)
+            {
+                pn.baseAddress = ushort.Parse(gvPorts.SelectedRows[0].Cells[0].Value.ToString(), System.Globalization.NumberStyles.HexNumber);
+            }
+        }
+
+    }
+}
